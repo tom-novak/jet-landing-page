ed0914c89762ad351ae3927454eac5f2048c6f15
Tomáš Novák <hello@tom-novak.com>
2021-01-03 19:44:54 +0100

    Responsive cards implemented

--- lib/jet_landing_page.dart
+++ lib/jet_landing_page.dart
@@ -1,11 +1,12 @@
 library jet_landing_page;
 
+export 'src/widget/card_container.dart';
+export 'src/widget/card_list.dart';
+export 'src/widget/card_stripe.dart';
+export 'src/widget/footer.dart';
 export 'src/widget/header.dart';
+export 'src/widget/page_cover.dart';
 export 'src/widget/parallax_image.dart';
 export 'src/widget/parallax_text.dart';
 export 'src/widget/simple_paragraph.dart';
 export 'src/widget/text_card.dart';
-export 'src/widget/card_stripe.dart';
-export 'src/widget/card_list.dart';
-export 'src/widget/page_cover.dart';
-export 'src/widget/footer.dart';
---
+++ lib/src/widget/card_container.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CardContainer extends StatelessWidget {
+  static const itemPadding = 48.0;
+
+  final List<Widget> children;
+
+  CardContainer(this.children);
+
+  List<Widget> _buildItems(BuildContext context) {
+    var cards = <Widget>[];
+    Widget container;
+    for (var item in children) {
+      container = Container(
+        padding: EdgeInsets.all(itemPadding / 2),
+        child: item,
+      );
+      if (MediaQuery.of(context).orientation == Orientation.landscape) {
+        container = Flexible(child: container);
+      }
+      cards.add(container);
+    }
+    return cards;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var portraitOrientation =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return Container(
+      padding: EdgeInsets.all(itemPadding / 2),
+      child: Flex(
+        direction: portraitOrientation ? Axis.vertical : Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: _buildItems(context),
+      ),
+    );
+  }
+}
