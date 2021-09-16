ab274d006892411a02c7307ba93c9564c5ca0b18
Tomáš Novák <hello@tom-novak.com>
2021-09-16 14:59:07 +0200

    Horizontal content padding on whole page on landscape implemented

--- example/lib/src/example_footer.dart
+++ example/lib/src/example_footer.dart
@@ -6,9 +6,12 @@ import 'package:jet_landing_page_example/src/links.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ExampleFooter extends StatelessWidget {
+  final double contentWidth;
+
   const ExampleFooter({
     Key key,
     @required this.sectionMap,
+    this.contentWidth,
   }) : super(key: key);
 
   final Map<PageSection, GlobalKey<State<StatefulWidget>>> sectionMap;
@@ -19,29 +22,40 @@ class ExampleFooter extends StatelessWidget {
       data: Theme.of(context).copyWith(
         textButtonTheme: footerLinkTheme,
       ),
-      child: Footer(
-        key: sectionMap[PageSection.about],
-        info: SimpleParagraph(
-          title: AppLocalizations.of(context).info,
-          titleStyle: Theme.of(context)
-              .copyWith(brightness: Brightness.dark)
-              .textTheme
-              .headline6,
-          text: AppLocalizations.of(context).loremIpsumDolorSitAmetLong,
-          textStyle: Theme.of(context).textTheme.bodyText2,
-          padding: EdgeInsets.zero,
-        ),
-        flatNavigation: SimpleParagraph(
-          title: AppLocalizations.of(context).flatNavigation,
-          titleStyle: Theme.of(context).textTheme.headline6,
-          text: AppLocalizations.of(context).loremIpsumDolorSitAmet,
-          textStyle: Theme.of(context).textTheme.bodyText2,
-          padding: EdgeInsets.zero,
-        ),
-        links: Links(),
-        copyright: Text(
-          AppLocalizations.of(context).jetLandingPageCopyright,
-          style: Theme.of(context).textTheme.bodyText2,
+      child: Container(
+        color: Theme.of(context).accentColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: contentWidth,
+              child: Footer(
+                key: sectionMap[PageSection.about],
+                info: SimpleParagraph(
+                  title: AppLocalizations.of(context).info,
+                  titleStyle: Theme.of(context)
+                      .copyWith(brightness: Brightness.dark)
+                      .textTheme
+                      .headline6,
+                  text: AppLocalizations.of(context).loremIpsumDolorSitAmetLong,
+                  textStyle: Theme.of(context).textTheme.bodyText2,
+                  padding: EdgeInsets.zero,
+                ),
+                flatNavigation: SimpleParagraph(
+                  title: AppLocalizations.of(context).flatNavigation,
+                  titleStyle: Theme.of(context).textTheme.headline6,
+                  text: AppLocalizations.of(context).loremIpsumDolorSitAmet,
+                  textStyle: Theme.of(context).textTheme.bodyText2,
+                  padding: EdgeInsets.zero,
+                ),
+                links: Links(),
+                copyright: Text(
+                  AppLocalizations.of(context).jetLandingPageCopyright,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
